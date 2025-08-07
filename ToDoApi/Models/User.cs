eee333be744cf09418cdea77a86c154aad31fab7
andre <aberdysev74@gmail.com>
2025-08-07 15:36:19 +0700

    Добавление класса User

---
+++ ToDoApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApi.Models;
+
+public class User
+{
+    public Guid Id { get; init; }
+    public string Email { get; init;} = string.Empty;
+    public string PasswordHash { get; init;} = string.Empty;
+    public string FullName { get; init; } = string.Empty;
+    public DateTime CreatedAt { get; init;}
+}
